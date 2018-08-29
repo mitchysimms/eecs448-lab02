db73e24ae6c5906cf0172041d8bbbff704da453c
Mitch Simmons <mitch.t.simmons@ku.edu>
2018-08-29 10:00:32 -0500

    Fixed search method to parse all nodes and return true if value is found

--- linkedList/LinkedList.hpp
+++ linkedList/LinkedList.hpp
@@ -39,9 +39,12 @@ bool LinkedList<T>::search(T value) const
 	Node<T>* temp = m_front;
 	bool isFound = false;
 
-	/** TODO
-		Fix this method
-	*/
+	while(temp != nullptr){
+		if(temp->getValue() == value){
+			isFound = true;
+		}
+		temp = temp->getNext();
+	}
 
 	return(isFound);
 }
